592e487ebc3516e60e0afeea63933109ef236df0
RESHMA <88847800+Reshma-Crazeee@users.noreply.github.com>
2021-12-16 13:06:41 +0530

    pdf change

--- src/components/modules/home/Home.js
+++ src/components/modules/home/Home.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import resume  from './Resume.pdf';
-import './Home.css';
+import React from "react";
+import { Link } from "react-scroll";
+import resume from "./Resume.pdf";
+import "./Home.css";
 
-export default function Home(){
-    return(
-        <div className="container">
-            <div className="inner">
-                <div className="intro">Hi,<div className="iam"> I am Reshma Kandregula</div></div>
-                <div className="role">A Front-end Developer</div>
-                <div className="tagline">Get Ready to turn ideas into reality</div >
-                <div className="btns">
-                    <Link to="contact" spy={true} smooth={true}>
-                    <button className="btn hire-me">Hire Me</button>
-                    </Link>
-                    <a href={resume} download="Reshma Resume" target="_blank" rel="noreferrer">
-                    <button className="btn get-resume">Get Resume</button>
-                    </a>
-                </div>
-            </div>
-            <div className="prof-pic">
-                <div className="prof-img">
-
-                </div>
-            </div>
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="inner">
+        <div className="intro">
+          Hi,<div className="iam"> I am Reshma Kandregula</div>
         </div>
-    )
+        <div className="role">A Front-end Developer</div>
+        <div className="tagline">Get Ready to turn ideas into reality</div>
+        <div className="btns">
+          <Link to="contact" spy={true} smooth={true}>
+            <button className="btn hire-me">Hire Me</button>
+          </Link>
+          <a
+            href={resume}
+            download="Reshma Resume"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="btn get-resume">Get Resume</button>
+          </a>
+        </div>
+      </div>
+      <div className="prof-pic">
+        <div className="prof-img"></div>
+      </div>
+    </div>
+  );
 }
